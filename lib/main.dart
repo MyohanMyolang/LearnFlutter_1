c393ab995f53850c360118ab5e28c8dc5e70dfe7
Myolang <myohanmyolang@gmail.com>
2023-02-21 10:14:56 +0900

    All remove prev Project Files And Create New File

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,16 @@
-import 'package:app1/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Pomodoro());
+  runApp(const Webtoon());
 }
 
-class Pomodoro extends StatelessWidget {
-  const Pomodoro({super.key});
+class Webtoon extends StatelessWidget {
+  const Webtoon({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        textTheme: const TextTheme(
-          displayLarge: TextStyle(
-            color: Color(0xFF232B55),
-          ),
-        ),
-        cardColor: const Color(0xFFF4EDDB),
-        //backgroundColor를 대신하여 쓴 코드이다.
-        scaffoldBackgroundColor: const Color(0xFFE7626C),
-      ),
-      home: const HomeScreen(),
+    return const MaterialApp(
+      home: Text("test"),
     );
   }
 }
